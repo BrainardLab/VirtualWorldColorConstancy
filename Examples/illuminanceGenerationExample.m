b95c885c2a33b5e4112e08e99aa3e696c46f41e8
vijaysoophie <vijaysoophie@gmail.com>
2016-11-15 09:11:28 -0500

    rescale and amplify illuminants
    rescale illuminants by the mean before sampling and multiply the newly generated illuminants by a random number for getting variation in spectra

--- Examples/illuminanceGenerationExample.m
+++ Examples/illuminanceGenerationExample.m
@@ -9,7 +9,7 @@ clear; %close all;
 
 % Desired wl sampling
 nIlluminances = 1000;
-rescaling = 0;  % O no rescaling
+rescaling = 1;  % O no rescaling
                 % 1 rescaling
 
 S = [400 5 61];
@@ -18,21 +18,9 @@ theWavelengths = SToWls(S);
 load daylightGranadaLong
 daylightGranadaOriginal = SplineSrf(S_granada,daylightGranada,S);
 
-% maxDaylightGranada = max(daylightGranadaOriginal);  
-% daylightGranadaRescaled = daylightGranadaOriginal./repmat(maxDaylightGranada,[size(daylightGranadaOriginal,1),1]);
-% meanDaylightGranada = mean(daylightGranadaOriginal);  
-% daylightGranadaRescaled = daylightGranadaOriginal./repmat(meanDaylightGranada,[size(daylightGranadaOriginal,1),1]);
-
-% From each spectra subtract its mean value
-% meanDaylightGranada = mean(daylightGranadaOriginal);  
-% daylightGranadaMeanCentered = bsxfun(@minus,daylightGranadaOriginal,meanDaylightGranada);
-
-% Normalize the spectra by their L2 norm
-lengthDaylightGranada = sqrt(sum(daylightGranadaOriginal.*daylightGranadaOriginal));  
-daylightGranadaRescaled = daylightGranadaOriginal./repmat(lengthDaylightGranada,[size(daylightGranadaOriginal,1),1]);
-
-% Add the mean value rescaled by the L2 norm
-% daylightGranadaRescaled = bsxfun(@plus,daylightGranadaRescaled,meanDaylightGranada./lengthDaylightGranada);
+% Rescale spectrum by its mean
+meanDaylightGranada = mean(daylightGranadaOriginal);
+daylightGranadaRescaled = bsxfun(@rdivide,daylightGranadaOriginal,meanDaylightGranada);
 
 % Center the data for PCA
 if ~ rescaling 
@@ -59,9 +47,10 @@ for i = 1:nNewIlluminaces
         ran_ill = B*ran_wgts+meandaylightGranadaRescaled;
         if (all(ran_ill >= 0))
             newIlluminance(:,newIndex) = ran_ill;
+            newIlluminance(:,newIndex) = newIlluminance(:,newIndex)*(rand*max(meanDaylightGranada));
             newIndex = newIndex+1;
             OK = true;
-        end
+        end        
     end
 end
 %%
@@ -103,10 +92,10 @@ ylimit=get(gca,'ylim');
 
 subplot(2,3,2)
 plot(SToWls(S),daylightGranadaRescaled);
-title('Daylight Granada Rescaled');
+title('Daylight Granada Rescaled By Mean');
 xlabel('Wavelength (nm)')
 ylabel('Rescaled Spectral Power Distribution')
-ylimit=get(gca,'ylim');
+% ylimit=get(gca,'ylim');
 ylim(ylimit);
 
 subplot(2,3,3)
@@ -118,23 +107,23 @@ ylabel('Rescaled Spectral Power Distribution')
 
 subplot(2,3,4)
 hold on;
-plot(IlluminantxyYGranada(1,:),IlluminantxyYGranada(2,:),'r.');
 plot(IlluminantxyY(1,:),IlluminantxyY(2,:),'b*');
+plot(IlluminantxyYGranada(1,:),IlluminantxyYGranada(2,:),'r.');
 legend('Randomly generated','Granada','Location', 'southeast');
 title('xy chromaticity diagram');
 xlabel('x')
 ylabel('y')
-legend('Granda Rescaled','New Illuminaces','Location', 'northwest');
+legend('New Illuminace','Granda Rescaled','Location', 'northwest');
 
 
 subplot(2,3,5)
 hold on;
-plot(ill_granada_wgts(1,:),ill_granada_wgts(2,:),'.');
 plot(wgts_New(1,:),wgts_New(2,:),'*');
+plot(ill_granada_wgts(1,:),ill_granada_wgts(2,:),'.');
 title('Projection along first two PCs');
 xlabel('PC1')
 ylabel('PC2')
-legend('Granda Rescaled','New Illuminaces','Location', 'northwest');
+legend('New Illuminace','Granda Rescaled','Location', 'northwest');
 
 subplot(2,3,6)
 image(theIlluminationImage);
